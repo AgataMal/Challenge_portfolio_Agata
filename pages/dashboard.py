d5b0329f62bb2c9760d4c9ad23baf455dacf3bc0
Agata Mlaczyk <malczyk.agata88@gmail.com>
2023-04-08 13:38:32 +0200

    subtask4

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,9 +1,28 @@
 from pages.base_page import BasePage
 
 class Dashboard(BasePage):
+    
     language_button_xpath = "//ul//div[@role='button' and ./div/span[text()='English']]"
+    
     players_button_xpath = "//ul//div[@role='button' and ./div/span[text()='Players']]"
+    
     main_page_button = "//ul//div[@role='button' and ./div/span[text()='Main page']]"
+    
     sign_out_button = "//ul//div[@role='button' and ./div/span[text()='Sign out']]"
+    
     add_player_button = "//div//a//span[text()='Dev team contact']"
+    
     dev_team_contact_button = "//div//a//span[text()='Dev team contact']"
+
+    """
+        User activity list links
+    """
+    users_activity_pattern = "//div[./h6/following-sibling::a]/a[{}]"
+    users_activity_list_1 = users_activity_pattern.format("1")
+    users_activity_list_2 = users_activity_pattern.format("2")
+    users_activity_list_3 = users_activity_pattern.format("3")
+    users_activity_list_4 = users_activity_pattern.format("4")
+    users_activity_list_5 = users_activity_pattern.format("5")
+
+
+
