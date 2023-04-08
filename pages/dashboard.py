afa0d29a5df279e4896d81d33318900f531ee531
Agata Mlaczyk <malczyk.agata88@gmail.com>
2023-04-08 13:46:06 +0200

    subtask4

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,13 +2,16 @@ from pages.base_page import BasePage
 
 class Dashboard(BasePage):
     
-    language_button_xpath = "//ul//div[@role='button' and ./div/span[text()='English']]"
+    left_menu_buttons_pattern = "//ul//div[@role='button' and ./div/span[text()='{}']]"
+
+    language_button_en_xpath = left_menu_buttons_pattern.format("English")
+        
+    players_button_en_xpath = left_menu_buttons_pattern.format("Players")    
     
-    players_button_xpath = "//ul//div[@role='button' and ./div/span[text()='Players']]"
+    main_page_en_button_xpath = left_menu_buttons_pattern.format("Main page")    
     
-    main_page_button = "//ul//div[@role='button' and ./div/span[text()='Main page']]"
-    
-    sign_out_button = "//ul//div[@role='button' and ./div/span[text()='Sign out']]"
+    sign_out_en_button_xpath = left_menu_buttons_pattern.format("Sign out")
+
     
     add_player_button = "//div//a//span[text()='Dev team contact']"
     
